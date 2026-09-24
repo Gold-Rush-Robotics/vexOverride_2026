a99ecb4c023fbac62df64294f5f1416a7b5d2dfd
pGovm <pgovindmenon07@gmail.com>
2026-09-24 16:46:02 -0400

    Changed some commits

--- 15Inch/src/drivebase.cpp
+++ 15Inch/src/drivebase.cpp
@@ -1,23 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       VEX                                                       */
-/*    Created:      Wed Sep 25 2019                                           */
-/*    Description:  Tank Drive                                                */
-/*    This sample allows you to control the V5 Clawbot using the both         */
-/*    joystick. Adjust the deadband value for more accurate movements.        */
-/*----------------------------------------------------------------------------*/
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller
-// LeftMotor            motor         1
-// RightMotor           motor         10
-// ClawMotor            motor         3
-// ArmMotor             motor         8
-// ---- END VEXCODE CONFIGURED DEVICES ----
-
 #include "vex.h"
 
 using namespace vex;
